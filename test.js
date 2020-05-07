86f11074eff8d87c222048dc1232500a6905ae5d
limseongkyu <96x60812@naver.com>
2020-05-07 20:13:36 +0900

    test

---
+++ test.js
@@ -0,0 +1,16 @@
+var str = 'abc';
+
+str.length = 1;
+
+console.log(str.length);
+
+str.foo = 3;
+console.log(str.foo);
+
+console.log("did you say \"hello\"?");
+
+var count = 3;
+
+var statement = 'you have' + ' ' +  count + ' ' + 'messages'
+
+console.log(statement);
