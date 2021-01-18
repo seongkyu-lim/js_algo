01cac9a6edbab92b08f9ad352dd9f76f7f4c7ef1
limseongkyu <96x60812@naver.com>
2021-01-18 22:24:30 +0900

    [edit]

---
+++ mdn_tutorial_js/Array/array.js
@@ -0,0 +1,19 @@
+const a = 1;
+
+let arr = [a,1,'tree']
+
+// console.log(arr)
+
+var myData = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
+
+let arr2 = myData.split(',')
+
+// console.log(arr2)
+
+let b = arr2.join('::')
+
+// console.log(b)
+
+let c = arr2.toString(',')
+
+console.log(c)
